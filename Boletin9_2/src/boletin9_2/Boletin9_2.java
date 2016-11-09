02356d375e09c1dd0e60384de0d0d948c13dc0e2
Adry <Adry@Sanisidro>
2016-11-09 09:13:21 +0100

    Final 9_2

--- Boletin9_2/src/boletin9_2/Boletin9_2.java
+++ Boletin9_2/src/boletin9_2/Boletin9_2.java
@@ -7,7 +7,9 @@ package boletin9_2;
 public class Boletin9_2 {
 
     public static void main(String[] args) {
-        
+       Metodo obx1=new Metodo();
+       obx1.suma();
+       obx1.multiplicacion();
     }
     
 }
--- Boletin9_2/src/boletin9_2/Metodo.java
+++ Boletin9_2/src/boletin9_2/Metodo.java
@@ -5,5 +5,15 @@ package boletin9_2;
  * @author Adry
  */
 public class Metodo {
-    
+    float suma=0;double multiplicacion=1;
+    public void suma(){
+        for(int i=10;i<=90;i++){
+            suma=suma+i;}
+        System.out.println("A suma dos numeros entre 10 e 90-> "+ suma);
+    }
+    public void multiplicacion(){
+        for (int i=10;i<=90;i++)
+            multiplicacion=multiplicacion*i;
+        System.out.println("A multiplicacion dos numeros entre 10 e 90-> "+multiplicacion);
+            }
 }
